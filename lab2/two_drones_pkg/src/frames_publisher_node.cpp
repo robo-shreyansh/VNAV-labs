a585d9b7cd28412b911fd7069a6a92c4c5847eeb
robo-shreyansh <shreyansh.sh99@gmail.com>
2025-11-04 03:50:44 +0530

    Lab 2 partial solution

--- lab2/two_drones_pkg/src/frames_publisher_node.cpp
+++ lab2/two_drones_pkg/src/frames_publisher_node.cpp
@@ -7,11 +7,16 @@
 #include <rclcpp/create_timer.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/timer.hpp>
+#include <cmath>
 
 class FramesPublisherNode : public rclcpp::Node {
   rclcpp::Time startup_time;
 
   rclcpp::TimerBase::SharedPtr heartbeat;
+  rclcpp::Logger logger = this->get_logger();
+  rclcpp::Publisher<geometry_msgs::msg::TransformStamped>::SharedPtr av1_publisher_;
+  rclcpp::Publisher<geometry_msgs::msg::TransformStamped>::SharedPtr av2_publisher_;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
   // TODO: Declare a std::unique_ptr<tf2_ros::TransformBroadcaster>
   // ...
@@ -29,6 +34,8 @@ class FramesPublisherNode : public rclcpp::Node {
                              rclcpp::Duration::from_seconds(0.02),
                              std::bind(&FramesPublisherNode::onPublish, this));
     heartbeat->reset();
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+
   }
 
   void onPublish() {
@@ -43,12 +50,19 @@ class FramesPublisherNode : public rclcpp::Node {
     //   - convert the resulting Duration to seconds, store result into a double
 
     // ...
+    
+    rclcpp::Duration elapsed_time = this->now() - startup_time;
+    double elapsed_sec = elapsed_time.seconds();
 
+    RCLCPP_INFO(logger, "Ok getting logged! %lf", elapsed_time.seconds());
+    
     // Here we declare two geometry_msgs::msg::TransformStamped objects, which
     // need to be populated
     geometry_msgs::msg::TransformStamped world_T_av1;
     geometry_msgs::msg::TransformStamped world_T_av2;
-
+    
+    
+    
     // 2. TODO: Populate the two transforms for the AVs, using the variable "time"
     //    computed above. Specifically:
     //     - world_T_av1 should have origin in [cos(time), sin(time), 0.0] and
@@ -68,6 +82,38 @@ class FramesPublisherNode : public rclcpp::Node {
     //    - consider the setRPY method on a tf2::Quaternion for world_T_av1
 
     // ...
+
+    tf2::Quaternion q1;
+    q1.setRPY(0, 0, elapsed_sec);
+    world_T_av1.header.stamp = this->get_clock()->now();
+    world_T_av1.header.frame_id = "world";
+    world_T_av1.child_frame_id = "av1";
+    world_T_av1.transform.translation.x = std::cos(elapsed_sec);
+    world_T_av1.transform.translation.y = std::sin(elapsed_sec);
+    world_T_av1.transform.translation.z = 0.0;
+    world_T_av1.transform.rotation.x = q1.x();
+    world_T_av1.transform.rotation.y = q1.y();
+    world_T_av1.transform.rotation.z = q1.z();
+    world_T_av1.transform.rotation.w = q1.w();
+    
+
+    tf2::Quaternion q2;
+    q2.setRPY(0, 0, 0);
+    world_T_av2.header.stamp = this->get_clock()->now();
+    world_T_av2.header.frame_id = "world";
+    world_T_av2.child_frame_id = "av2";
+    world_T_av2.transform.translation.x = std::sin(elapsed_sec);
+    world_T_av2.transform.translation.y = 0.0;
+    world_T_av2.transform.translation.z = std::cos(2 * elapsed_sec);
+    world_T_av2.transform.rotation.x = q2.x();
+    world_T_av2.transform.rotation.y = q2.y();
+    world_T_av2.transform.rotation.z = q2.z();
+    world_T_av2.transform.rotation.w = q2.w();
+
+    // av1_publisher_->publish(world_T_av1);
+    // av2_publisher_->publish(world_T_av2);
+    tf_broadcaster_->sendTransform(world_T_av1);
+    tf_broadcaster_->sendTransform(world_T_av2);
 
     // 3. TODO: Publish the transforms, namely:
     //     - world_T_av1 with frame_id "world", child_frame_id "av1"
